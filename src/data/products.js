acf7d875a463fb1ba9948d632f5de83e7dc4f253
JohannaMRoa <njmontiel96@gmail.com>
2026-05-11 22:25:41 +0100

    11 may

---
+++ src/data/products.js
@@ -0,0 +1,19 @@
+export const products = [
+  {
+    id: 1,
+    title: "Children of Lir Art Tee",
+    category: "T-Shirts",
+    price: 24.99,
+    image: "/shop/lir-tee.png",
+    tag: "New",
+  },
+
+  {
+    id: 2,
+    title: "Giant’s Causeway Figure",
+    category: "Figures",
+    price: 34.99,
+    image: "/shop/giant-figure.png",
+    tag: "Featured",
+  },
+];
